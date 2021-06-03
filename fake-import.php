0e6b139cd9bd03c86b40eeb175c9d769973203bd
Grant MacDonald <phisoluciones.es@gmail.com>
2021-06-03 14:36:36 +0200

    Fake importer

---
+++ fake-import.php
@@ -0,0 +1,28 @@
+<?php
+    $date = new \DateTime();
+    $data = array(
+        'node_id' => 'test_node',
+        'temp_external' => rand(30, 100),
+        'light_external' => rand(30, 100),
+        'humidity_external' => rand(30, 100),
+        'differential_potenial_ch1' => rand(30, 100),
+        'differential_potenial_ch2' => rand(30, 100),
+        'rf_power_emission' => rand(30, 100),
+        'transpiration' => rand(30, 100),
+        'air_pressure' => rand(30, 100),
+        'soil_moisture' => rand(30, 100),
+        'soil_temperature' => rand(30, 100),
+        'date' => $date->format('Y-m-d H:i:s')
+    );
+    $ch = @curl_init();
+    @curl_setopt($ch, CURLOPT_POST, true);
+    @curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    @curl_setopt($ch, CURLOPT_URL, $endpoint);
+	@curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+    @curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        'Content-Type: application/json'
+    ));
+    @curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $response    = @curl_exec($ch);
+    @curl_close($ch);
+?>
